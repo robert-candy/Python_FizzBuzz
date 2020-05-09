14469667d3ade269e98be9be2abab63c35e298ea
robert-candy <candyrobertt@gmail.com>
2020-05-09 20:59:55 +0300

    first commit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,14 @@
-This is first line code 
+n = int(input('Please enter an range end point: ')) 
+FzBz_list = []  
+
+for num in range(1,n+1): 
+    if num % 3 == 0 and num % 5 == 0:
+        FzBz_list.append('FizzBuzz')
+    elif num % 5 == 0:
+        FzBz_list.append('Buzz')
+    elif num % 3 == 0:
+        FzBz_list.append('Fizz')
+    else:
+         FzBz_list.append(num)  
+print(FzBz_list)
+
